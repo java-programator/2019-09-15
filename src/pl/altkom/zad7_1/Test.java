6c527401ae09a3ec92e8f2e58d5bbd52a29234f1
paulina <paulina.a.wojtal@gmail.com>
2019-09-15 15:53:36 +0200

    Zadania 7.1

---
+++ src/pl/altkom/zad7_1/Test.java
@@ -0,0 +1,16 @@
+package pl.altkom.zad7_1;
+
+// klasa testowa
+public class Test {
+
+	// metoda glowna
+	public static void main(String[] args) {
+		Employee emp = new Manager("Adam");
+		System.out.println(emp.getInfo());
+		System.out.println(emp.getEmployeeInfo());
+
+		Manager mg = new Manager("Janek");
+		System.out.println(mg.getInfo());
+		System.out.println(mg.getEmployeeInfo());
+	}
+}
