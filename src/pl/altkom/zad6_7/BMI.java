73c6aa578cec7d4b61dae9f07fa5f6f2b0509dce
paulina <paulina.a.wojtal@gmail.com>
2019-09-15 10:08:47 +0200

    Zadanie 6.7 dodatkowa metoda

--- src/pl/altkom/zad6_7/BMI.java
+++ src/pl/altkom/zad6_7/BMI.java
@@ -22,6 +22,20 @@ public class BMI {
         return result;
     }
 
+    public static String bmiDiagnose2(double weight, double height){
+        double bmi = bmiCounter(weight, height);
+        String result = "";
+        if(bmi < 18.5){
+            result = "Masz niedowagę";
+            // nie ma potrzeby dawać kolejnego przedziału jako bmi >= 18.5 && bmi <= 24.99, bo to wynika z tego, że program przeszedł już do else ifa
+        } else if(bmi <= 24.99){
+            result = "Twój stosunek masy ciała do wzrostu jest prawidłowy";
+        } else {
+            result = "Masz nadwagę";
+        }
+        return result;
+    }
+
     // wpisujemy z przecinkiem, ale w programie trzeba używać kropki jako separatora
     // przecinek jest separatorem dla Polski
     public static void main(String[] args) {
@@ -32,6 +46,8 @@ public class BMI {
         double height = sc.nextDouble();
         System.out.println(bmiCounter(weight, height));
 
-        System.out.println(bmiDiagnose(bmiCounter(weight, height)));
+        //System.out.println(bmiDiagnose(bmiCounter(weight, height)));
+
+        System.out.println(bmiDiagnose2(weight, height));
     }
 }
