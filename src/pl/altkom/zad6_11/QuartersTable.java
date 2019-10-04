1dc49e27f0798da5a991fbbe0e7b61a0a15bf841
twish <t.wiszynski@gmail.com>
2019-10-04 22:41:51 +0200

    zad6_11 added

--- src/pl/altkom/zad6_11/QuartersTable.java
+++ src/pl/altkom/zad6_11/QuartersTable.java
@@ -1,26 +1,53 @@
 package pl.altkom.zad6_11;
 
+import java.util.Arrays;
 
 public class QuartersTable {
 
-    private static String quarters[] = {"Q1", "Q2", "Q3", "Q4"};
-    private static String months[] = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
-
-    static String[][] quarterRow(){
-        int rows = 4;
-        int columns = 4;
-        String qRow[][] = new String[rows][columns];
-        for(int i = 0; i < rows; i++){
-            qRow[i][0] = quarters[i];
-            qRow[i][1] = months[i*3];
-            qRow[i][2] = months[i*3+1];
-            qRow[i][3] = months[i*3+2];
+    public static void kwartalyWiersze(String[] headers, String[] columns){
+        String tab1[][] = new String[4][4];
+        int q = 0;
+        int m = 0;
+        for (int i = 0; i < tab1.length; i++){
+            for (int j = 0; j < tab1[i].length; j++){
+                if (j == 0){
+                    tab1[i][j] = headers[q];
+                    q++;
+                }
+                else {
+                    tab1[i][j] = columns[m];
+                    m++;
+                }
+            }
+            System.out.println(Arrays.toString(tab1[i]));
         }
-        return qRow;
     }
+
+//    public static void kwartalyKolumny(String[] headers, String[] columns){
+//        String tab1[][] = new String[4][4];
+//        int q = 0;
+//        int m = 0;
+//        for (int i = 0; i < tab1.length; i++){
+//            for (int j = 0; j < tab1[i].length; j++){
+//                if (i == 0){
+//                    tab1[i][j] = headers[q];
+//                    q++;
+//                }
+//                else {
+//                    tab1[i][j] = columns[];
+//                }
+//            }
+//            System.out.println(Arrays.toString(tab1[i]));
+//        }
+//    }
+
 
     public static void main(String[] args) {
-        System.out.println();
-    }
+        String quarters[] = {"[Q1]", "[Q2]", "[Q3]", "[Q4]"};
+        String months[] = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
 
+        kwartalyWiersze(quarters, months);
+
+
+    }
 }
